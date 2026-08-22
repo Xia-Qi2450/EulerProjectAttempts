c677a811ea85ecab6709aa8a26473ff59be79b8d
Xia Qi <xiaqihill2010@gmail.com>
2026-08-22 22:32:24 +0800

    chore: bump version

--- euler_problems/solver.py
+++ euler_problems/solver.py
@@ -75,7 +75,7 @@ class EulerSolver(
     def __init__(self, easter_eggs=True, forced_easter_eggs=False, spinners_enabled=True) -> None:
         import colorama
         colorama.init(autoreset=True)
-        self.VERSION = "1.0_b"
+        self.VERSION = "1.1"
         self.GOAL = "200"
         self.EASTER_EGGS: bool = easter_eggs
         self.FORCED_EASTER_EGGS: bool = forced_easter_eggs
